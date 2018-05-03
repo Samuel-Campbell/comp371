5f4aa5f138912523a3ac9030a6b7cb8ed5a61828
samuel-campbell <samuel.pcampbell@gmail.com>
2018-05-03 00:31:04 -0400

    initial commit

---
+++ cmake-build-debug/CMakeFiles/comp371.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/comp371.dir/main.cpp.o"
+  "comp371.pdb"
+  "comp371"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/comp371.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
