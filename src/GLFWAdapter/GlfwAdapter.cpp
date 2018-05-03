1d0d4f90f3864c8e877e34ceccdad9a095f54aaa
samuel-campbell <samuel.pcampbell@gmail.com>
2018-05-03 14:56:16 -0400

    testing vertices

--- src/GLFWAdapter/GlfwAdapter.cpp
+++ src/GLFWAdapter/GlfwAdapter.cpp
@@ -53,6 +53,7 @@ bool GlfwAdapter::init() {
  * Safely terminate GLFW at the end
  */
 void GlfwAdapter::run(){
+    //vertexShader();
     while(!glfwWindowShouldClose(window)) {
         processInput(window);
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -84,4 +85,40 @@ void GlfwAdapter::framebuffer_size_callback(GLFWwindow *window, int width, int h
 void GlfwAdapter::processInput(GLFWwindow *window) {
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+}
+
+void GlfwAdapter::vertexInput() {
+    float vertices[] = {
+            -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.0f,  0.5f, 0.0f
+    };
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+}
+
+void GlfwAdapter::vertexShader() {
+    const char* vertexShaderSource = "\n"
+                                     "#version 330 core\n"
+                                     "layout (location = 0) in vec3 aPos;\n"
+                                     "\n"
+                                     "void main()\n"
+                                     "{\n"
+                                     "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                     "}";
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success){
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
 }
--- src/GLFWAdapter/GlfwAdapter.h
+++ src/GLFWAdapter/GlfwAdapter.h
@@ -14,6 +14,8 @@ public:
 private:
     static void framebuffer_size_callback(GLFWwindow* window, int width, int height);
     void processInput(GLFWwindow *window);
+    void vertexInput();
+    void vertexShader();
 
     const int OPENGL_VERSION = 3;
     const int WINDOW_WIDTH = 800;
